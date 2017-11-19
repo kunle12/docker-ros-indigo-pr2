1e62ea3b381fae8b8ec24022e7733c069275e9b7
Xun Wang <wang.xun@gmail.com>
2017-11-19 22:59:01 +1100

    enable local env.

--- assets/etc/profile.d/ros-setup.sh
+++ assets/etc/profile.d/ros-setup.sh
@@ -1,8 +1,8 @@
 if [ ! -z "$BASH" ]; then
     . /opt/ros/${ROS_DISTRO}/setup.bash
-    #. /home/pr2/dev/catkin_ws/devel/setup.bash
+    . /home/pr2/dev/catkin_ws/devel/setup.bash
 
 else
     . /opt/ros/${ROS_DISTRO}/setup.sh
-    #. /home/pr2/dev/catkin_ws/devel/setup.sh
+    . /home/pr2/dev/catkin_ws/devel/setup.sh
 fi
